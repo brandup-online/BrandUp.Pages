d995a59750f11e739e16b20555de8ea51483a69b
Dmitry <d.kovyazin@hotmail.com>
2019-05-06 13:57:35 +0700

    Delete type script generated files.

--- examples/LandingWebSite/_client/root.js
+++
@@ -1,5 +0,0 @@
-define(["require", "exports", "./styles.less"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-});
-//# sourceMappingURL=root.js.map
