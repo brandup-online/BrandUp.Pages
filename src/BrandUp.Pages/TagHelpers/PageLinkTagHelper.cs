63e2af841b74d8f10433d39e838c592476e75f50
Dmitry <d.kovyazin@hotmail.com>
2019-05-16 04:19:10 +0700

    Implemented tag helper by attribute asp-page-path for generation page url by path.

---
+++ src/BrandUp.Pages/TagHelpers/PageLinkTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using BrandUp.Pages.Url;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Threading.Tasks;
+
+namespace BrandUp.Pages.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes = "asp-page-path", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class PageLinkTagHelper : TagHelper
+    {
+        private readonly IPageLinkGenerator pageLinkGenerator;
+
+        [HtmlAttributeName("asp-page-path")]
+        public string PagePath { get; set; }
+
+        public PageLinkTagHelper(IPageLinkGenerator pageLinkGenerator)
+        {
+            this.pageLinkGenerator = pageLinkGenerator ?? throw new ArgumentNullException(nameof(pageLinkGenerator));
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var pagePath = await pageLinkGenerator.GetUrlAsync(PagePath);
+
+            output.Attributes.SetAttribute("href", pagePath);
+        }
+    }
+}
