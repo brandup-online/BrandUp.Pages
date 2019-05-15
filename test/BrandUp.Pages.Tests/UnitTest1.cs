53e5c123b2cc24627feac9d69fcb9be8aaacf94c
Dmitry <d.kovyazin@hotmail.com>
2019-05-16 01:52:00 +0700

    Commented tests in BrandUp.Pages.Tests.

--- test/BrandUp.Pages.Tests/UnitTest1.cs
+++ test/BrandUp.Pages.Tests/UnitTest1.cs
@@ -2,8 +2,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
-using System.Net;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace BrandUp.Pages.Tests
@@ -17,28 +15,28 @@ namespace BrandUp.Pages.Tests
             this.factory = factory;
         }
 
-        [Fact]
-        public async Task Test1()
-        {
-            using (var client = factory.CreateClient())
-            {
-                var response = await client.GetAsync("/");
+        //[Fact]
+        //public async Task Test1()
+        //{
+        //    using (var client = factory.CreateClient())
+        //    {
+        //        var response = await client.GetAsync("/");
 
-                Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
-            }
-        }
+        //        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        //    }
+        //}
 
-        [Fact]
-        public void Test2()
-        {
-            using (var client = factory.CreateClient())
-            {
-                using (var scope = factory.Server.Host.Services.CreateScope())
-                {
-                    var viewLocator = scope.ServiceProvider.GetRequiredService<Views.IViewLocator>();
-                }
-            }
-        }
+        //[Fact]
+        //public void Test2()
+        //{
+        //    using (var client = factory.CreateClient())
+        //    {
+        //        using (var scope = factory.Server.Host.Services.CreateScope())
+        //        {
+        //            var viewLocator = scope.ServiceProvider.GetRequiredService<Views.IViewLocator>();
+        //        }
+        //    }
+        //}
     }
 
     public class CustomWebApplicationFactory : WebApplicationFactory<LandingWebSite.Startup>
