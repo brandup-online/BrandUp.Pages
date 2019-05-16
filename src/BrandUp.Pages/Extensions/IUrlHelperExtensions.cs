b844fd6001a4d535122eaeb7c52178071d9cf5a7
Dmitry <d.kovyazin@hotmail.com>
2019-05-16 10:01:48 +0700

    Implemented method IUrlHelper.PagePathAsync.

---
+++ src/BrandUp.Pages/Extensions/IUrlHelperExtensions.cs
@@ -0,0 +1,15 @@
+﻿using BrandUp.Pages.Url;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Mvc
+{
+    public static class IUrlHelperExtensions
+    {
+        public static async Task<string> PagePathAsync(this IUrlHelper urlHelper, string path)
+        {
+            var pageLinkGenerator = urlHelper.ActionContext.HttpContext.RequestServices.GetRequiredService<IPageLinkGenerator>();
+            return await pageLinkGenerator.GetUrlAsync(path);
+        }
+    }
+}
