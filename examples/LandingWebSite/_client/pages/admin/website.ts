09642e87a8345ab19ae81ab5222b09c9693827e4
Dmitry <d.kovyazin@hotmail.com>
2019-05-22 01:56:02 +0700

    Implemented navigation to parent page in admin UI.

--- examples/LandingWebSite/_client/pages/admin/website.ts
+++ examples/LandingWebSite/_client/pages/admin/website.ts
@@ -1,7 +1,8 @@
-﻿import { UIElement, DOM } from "brandup-ui";
+﻿import { UIElement, DOM, ajaxRequest } from "brandup-ui";
 import Page from "../pages/page";
 import { listPageCollection } from "../dialogs/page-collection-list";
 import ContentPage from "../pages/content";
+import iconBack from "../svg/toolbar-button-back.svg";
 import iconList from "../svg/toolbar-button-list.svg";
 import iconTree from "../svg/toolbar-button-tree.svg";
 import { browserPage } from "../dialogs/page-browser";
@@ -12,23 +13,47 @@ export class WebSiteToolbar extends UIElement {
     constructor(page: Page<any>) {
         super();
 
-        var toolbarElem = DOM.tag("div", { class: "brandup-pages-elem brandup-pages-toolbar" }, [
-            DOM.tag("button", { class: "brandup-pages-toolbar-button list", "data-command": "brandup-pages-collections" }, iconList),
-            DOM.tag("button", { class: "brandup-pages-toolbar-button list", "data-command": "brandup-pages-collections2" }, iconTree)
-        ]);
+        var isContentPage = page instanceof ContentPage;
+        var buttons: Array<HTMLElement> = [];
+
+        if (isContentPage && (<ContentPage>page).model.parentPageId) {
+            buttons.push(DOM.tag("button", { class: "brandup-pages-toolbar-button", "data-command": "brandup-pages-back" }, iconBack));
+        }
+
+        buttons.push(DOM.tag("button", { class: "brandup-pages-toolbar-button", "data-command": "brandup-pages-collections" }, iconList));
+
+        if (isContentPage) {
+            buttons.push(DOM.tag("button", { class: "brandup-pages-toolbar-button", "data-command": "brandup-pages-collections2" }, iconTree));
+        }
+
+        var toolbarElem = DOM.tag("div", { class: "brandup-pages-elem brandup-pages-toolbar" }, buttons);
         document.body.appendChild(toolbarElem);
         this.setElement(toolbarElem);
-        
+
+        this.registerCommand("brandup-pages-back", () => {
+            let parentPageId: string = null;
+            if (isContentPage)
+                parentPageId = (<ContentPage>page).model.parentPageId;
+            if (parentPageId) {
+                ajaxRequest({
+                    url: `/brandup.pages/page/${parentPageId}`,
+                    success: (pageModel: PageModel) => {
+                        page.app.navigate(pageModel.url);
+                    }
+                });
+            }
+        });
+
         this.registerCommand("brandup-pages-collections", () => {
             let parentPageId: string = null;
-            if (page instanceof ContentPage)
+            if (isContentPage)
                 parentPageId = (<ContentPage>page).model.parentPageId;
             browserPage(parentPageId);
         });
 
         this.registerCommand("brandup-pages-collections2", () => {
             let parentPageId: string = null;
-            if (page instanceof ContentPage)
+            if (isContentPage)
                 parentPageId = (<ContentPage>page).model.id;
             listPageCollection(parentPageId);
         });
