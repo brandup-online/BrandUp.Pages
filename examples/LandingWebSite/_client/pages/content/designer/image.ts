69a2fa50da32d4e7165c3a44f2974b34062b1398
Dmitry <d.kovyazin@hotmail.com>
2019-05-28 16:05:29 +0700

    Implemented paste and drag image file in image designer.

--- examples/LandingWebSite/_client/pages/content/designer/image.ts
+++ examples/LandingWebSite/_client/pages/content/designer/image.ts
@@ -1,18 +1,29 @@
 ﻿import { FieldDesigner } from "./base";
 import { DOM } from "brandup-ui";
 import "./image.less";
-import iconUpload from "../../svg/upload-file.svg";
+import iconUpload from "../../svg/toolbar-button-picture.svg";
 import { ImageFieldValue, ImageFieldOptions } from "../field/image";
 
 export class ImageDesigner extends FieldDesigner<ImageFieldOptions> {
     private __fileInputElem: HTMLInputElement;
     private __button: HTMLElement;
+    private __closeFunc: (e: MouseEvent) => void;
 
     get typeName(): string { return "BrandUpPages.ImageDesigner"; }
     protected onRender(elem: HTMLElement) {
         elem.classList.add("image-designer");
 
-        elem.appendChild(this.__button = DOM.tag("button", { class: "image-designer-upload", "data-command": "select-file" }, iconUpload));
+        let menuElem: HTMLElement;
+        let textInput: HTMLInputElement;
+        elem.appendChild(menuElem = DOM.tag("div", { class: "bp-elem image-designer-menu" }, [
+            DOM.tag("ul", { class: "field-designer-popup" }, [
+                DOM.tag("li", null, DOM.tag("a", { href: "", "data-command": "open-editor" }, "Редактор")),
+                DOM.tag("li", { class: "split" }),
+                DOM.tag("li", null, DOM.tag("a", { href: "", "data-command": "upload-file" }, "Загрузить с компьютера"))
+            ]),
+            this.__button = DOM.tag("button", null, iconUpload),
+            textInput = <HTMLInputElement>DOM.tag("input", { type: "text" })
+        ]));
 
         this.__fileInputElem = <HTMLInputElement>DOM.tag("input", { type: "file" })
         this.__fileInputElem.addEventListener("change", () => {
@@ -21,15 +32,83 @@ export class ImageDesigner extends FieldDesigner<ImageFieldOptions> {
 
             this.__uploadFile(this.__fileInputElem.files.item(0));
             
-            this.__button.focus();
+            textInput.focus();
         });
+
+        textInput.addEventListener("paste", (e: ClipboardEvent) => {
+            e.preventDefault();
+
+            if (e.clipboardData.files.length > 0) {
+                this.__uploadFile(e.clipboardData.files.item(0));
+            }
+            else if (e.clipboardData.types.indexOf("text/plain") >= 0) {
+                let url = e.clipboardData.getData("text");
+                if (url && url.toLocaleLowerCase().startsWith("http"))
+                    this.__uploadFile(url);
+            }
+
+            textInput.focus();
+
+            elem.classList.remove("opened-menu");
+            document.body.removeEventListener("click", this.__closeFunc, false);
+        });
+
+        this.__closeFunc = (e: MouseEvent) => {
+            let t = <Element>e.target;
+            if (!t.closest(".image-designer-menu"))
+                elem.classList.remove("opened-menu");
+        };
 
         this.__button.addEventListener("click", (e: MouseEvent) => {
             e.preventDefault();
-            e.stopPropagation();
+            e.stopImmediatePropagation();
 
+            if (!elem.classList.toggle("opened-menu")) {
+                document.body.removeEventListener("click", this.__closeFunc, false);
+                return;
+            }
+
+            document.body.addEventListener("click", this.__closeFunc, false);
+            textInput.focus();
+        });
+
+        this.registerCommand("upload-file", () => {
+            elem.classList.remove("opened-menu");
+            document.body.removeEventListener("click", this.__closeFunc, false);
             this.__fileInputElem.click();
         });
+
+        var dragleaveTime = 0;
+        elem.ondragover = () => {
+            clearTimeout(dragleaveTime);
+            elem.classList.add("draging");
+            return false;
+        };
+        elem.ondragleave = () => {
+            dragleaveTime = window.setTimeout(() => { elem.classList.remove("draging"); }, 50);
+            return false;
+        };
+        elem.ondrop = (e: DragEvent) => {
+            e.stopPropagation();
+            e.preventDefault();
+
+            elem.classList.remove("draging");
+
+            let file = e.dataTransfer.files.item(0);
+            if (!file.type)
+                return false;
+
+            this.__uploadFile(file);
+
+            if (e.dataTransfer.items)
+                e.dataTransfer.items.clear();
+            else
+                e.dataTransfer.clearData();
+
+            textInput.focus();
+
+            return false;
+        };
     }
 
     hasValue(): boolean {
@@ -39,12 +118,10 @@ export class ImageDesigner extends FieldDesigner<ImageFieldOptions> {
     private __uploadFile(file: File | string) {
         this.element.classList.add("uploading");
 
+        let width = this.element.getAttribute("content-image-width");
+        let height = this.element.getAttribute("content-image-height");
+
         if (file instanceof File) {
-            let fileObject = <File>file;
-
-            let width = this.element.getAttribute("content-image-width");
-            let height = this.element.getAttribute("content-image-height");
-
             this.request({
                 url: `/brandup.pages/content/image`,
                 urlParams: {
@@ -53,12 +130,11 @@ export class ImageDesigner extends FieldDesigner<ImageFieldOptions> {
                     height: height
                 },
                 method: "POST",
-                data: fileObject,
+                data: <File>file,
                 success: (data: string, status: number) => {
                     switch (status) {
                         case 200:
                             this.element.style.backgroundImage = `url(${data})`;
-
                             this.element.classList.remove("uploading");
 
                             break;
@@ -71,11 +147,31 @@ export class ImageDesigner extends FieldDesigner<ImageFieldOptions> {
             return;
         }
         else if (typeof file === "string") {
-            let url = <string>file;
-            console.log(url);
+            this.request({
+                url: `/brandup.pages/content/image/url`,
+                urlParams: {
+                    url: <string>file,
+                    width: width,
+                    height: height
+                },
+                method: "POST",
+                success: (data: string, status: number) => {
+                    switch (status) {
+                        case 200:
+                            this.element.style.backgroundImage = `url(${data})`;
+                            break;
+                    }
+
+                    this.element.classList.remove("uploading");
+                }
+            });
         }
         else {
             this.element.classList.remove("uploading");
         }
     }
+}
+
+class ImageEditor {
+
 }
