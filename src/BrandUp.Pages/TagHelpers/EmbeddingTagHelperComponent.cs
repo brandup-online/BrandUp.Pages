bb03930c3912ece98ee53d4ed16f0b75c4233b3d
Dmitry <d.kovyazin@hotmail.com>
2019-08-02 08:08:17 +0700

    Embed page css class in render view.

--- src/BrandUp.Pages/TagHelpers/EmbeddingTagHelperComponent.cs
+++ src/BrandUp.Pages/TagHelpers/EmbeddingTagHelperComponent.cs
@@ -20,13 +20,30 @@ namespace BrandUp.Pages.TagHelpers
 
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
-            if (string.Equals(context.TagName, "body", StringComparison.OrdinalIgnoreCase))
+            if (ViewContext.ViewData.Model is AppPageModel appPageModel)
             {
-                if (ViewContext.ViewData.Model is AppPageModel appPageModel)
+                if (string.Equals(context.TagName, "body", StringComparison.OrdinalIgnoreCase))
+                {
+                    if (!string.IsNullOrEmpty(appPageModel.CssClass))
+                    {
+                        string cssClass = null;
+                        if (output.Attributes.TryGetAttribute("class", out TagHelperAttribute attribute))
+                            cssClass = (string)attribute.Value;
+
+                        if (!string.IsNullOrEmpty(cssClass))
+                            cssClass += " " + appPageModel.CssClass;
+                        else
+                            cssClass = appPageModel.CssClass;
+
+                        output.Attributes.SetAttribute("class", cssClass);
+                    }
+                }
+
+                if (string.Equals(context.TagName, "head", StringComparison.OrdinalIgnoreCase))
                 {
                     var appClientModel = await appPageModel.GetAppClientModelAsync(ViewContext.HttpContext.RequestAborted);
 
-                    output.PreContent.AppendHtml("<script type=\"text/javascript\">var appInitOptions = " + jsonHelper.Serialize(appClientModel) + "</script>");
+                    output.PostContent.AppendHtml("<script type=\"text/javascript\">var appInitOptions = " + jsonHelper.Serialize(appClientModel) + "</script>");
                 }
             }
         }
