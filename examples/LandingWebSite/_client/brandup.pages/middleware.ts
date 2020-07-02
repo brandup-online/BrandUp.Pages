5bd6aca628847888e138451ea09bc38e1238bc0c
Dmitry Kovyazin <it@brandup.online>
2020-07-02 12:40:17 +0700

    Updated NuGet and NPM packages.

--- examples/LandingWebSite/_client/brandup.pages/middleware.ts
+++ examples/LandingWebSite/_client/brandup.pages/middleware.ts
@@ -1,25 +1,32 @@
-﻿import { Middleware, NavigateContext, NavigatingContext } from "brandup-ui-app";
+﻿import { Middleware, NavigateContext, NavigatingContext, StartContext } from "brandup-ui-app";
 import { Page, PageModel } from "brandup-ui-website";
 import ContentPage from "./pages/content";
 import "./styles.less";
 
 export class ContentMiddleware extends Middleware {
-    navigating(context: NavigatingContext, next) {
+    start(context: StartContext, next) {
         next();
 
+        this._showUI(context.items);
+    }
+    navigating(context: NavigatingContext, next) {
         //if (context.items["nav"].enableAdministration) {
-            //const page = context.items["page"] as Page<PageModel>;
-            //if (page.model.editId)
-            //    context.isCancel = true;
+        //    const page = context.items["page"] as Page<PageModel>;
+        //    if (page.model.editId)
+        //        context.isCancel = true;
         //}
+
+        next();
     }
     navigate(context: NavigateContext, next) {
         next();
 
-        console.log(context.items);
+        this._showUI(context.items);
+    }
 
-        if (context.items["nav"].enableAdministration) {
-            const page = context.items["page"] as Page<PageModel>;
+    private _showUI(items: { [key: string]: any }) {
+        if (items["nav"].enableAdministration) {
+            const page = items["page"] as Page<PageModel>;
             if (!page.model.editId) {
                 import("./admin/website").then(d => {
                     page.attachDestroyElement(new d.WebSiteToolbar(page));
