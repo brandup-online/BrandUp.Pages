e6cb7499d3d8b588b1fbbcb1c4f0aaed392f3e4d
Dmitry Kovyazin <it@brandup.online>
2022-12-27 00:20:32 +0700

    Update NuGet

---
+++ examples/LandingWebSite/Pages/Blog/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using BrandUp.Website.Pages;
+using System.Threading.Tasks;
+
+namespace LandingWebSite.Pages.Blog
+{
+    public class IndexModel : AppPageModel
+    {
+        public override string Title => "About";
+        public override string Description => "About page description";
+        public override string Keywords => "about, company";
+        public override string ScriptName => "about";
+        public override string CssClass => "about-page";
+
+        protected override Task OnPageRequestAsync(PageRequestContext context)
+        {
+            //SetOpenGraph(Url.ContentLink("~/images/banner.jpg"), Title, Description);
+
+            return base.OnPageRequestAsync(context);
+        }
+    }
+}
