101d2fdc19395b94e88d7047dc871c849cd1c992
Dmitry Kovyazin <it@brandup.online>
2024-05-27 18:15:11 +0700

    Рендеринг статического блока

--- src/BrandUp.Pages.Core/ContentContext.cs
+++ src/BrandUp.Pages.Core/ContentContext.cs
@@ -4,42 +4,42 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace BrandUp.Pages
 {
-	public class ContentContext
-	{
-		public IPage Page { get; }
-		public ContentExplorer Explorer { get; }
-		public IServiceProvider Services { get; }
-		public object Content => Explorer.Model;
-		public bool IsDesigner { get; }
+    public class ContentContext
+    {
+        public IPage Page { get; }
+        public ContentExplorer Explorer { get; }
+        public IServiceProvider Services { get; }
+        public object Content => Explorer.Model;
+        public bool IsDesigner { get; }
 
-		public ContentContext(IPage page, object pageContent, IServiceProvider services, bool isDesigner)
-		{
-			if (pageContent == null)
-				throw new ArgumentNullException(nameof(pageContent));
+        public ContentContext(IPage page, object contentModel, IServiceProvider services, bool isDesigner)
+        {
+            ArgumentNullException.ThrowIfNull(contentModel);
 
-			Page = page ?? throw new ArgumentNullException(nameof(page));
-			Services = services ?? throw new ArgumentNullException(nameof(services));
+            Page = page;
+            Services = services ?? throw new ArgumentNullException(nameof(services));
 
-			var contentMetadataManager = services.GetRequiredService<IContentMetadataManager>();
+            var contentMetadataManager = services.GetRequiredService<IContentMetadataManager>();
 
-			Explorer = ContentExplorer.Create(contentMetadataManager, pageContent);
-			IsDesigner = isDesigner;
-		}
-		private ContentContext(ContentContext parent, ContentExplorer contentExplorer)
-		{
-			Page = parent.Page;
-			Services = parent.Services;
-			Explorer = contentExplorer;
-			IsDesigner = parent.IsDesigner;
-		}
+            Explorer = ContentExplorer.Create(contentMetadataManager, contentModel);
+            IsDesigner = isDesigner;
+        }
 
-		public ContentContext Navigate(string path)
-		{
-			var explorer = Explorer.Navigate(path);
-			if (explorer == null)
-				return null;
+        private ContentContext(ContentContext parent, ContentExplorer contentExplorer)
+        {
+            Page = parent.Page;
+            Services = parent.Services;
+            Explorer = contentExplorer;
+            IsDesigner = parent.IsDesigner;
+        }
 
-			return new ContentContext(this, explorer);
-		}
-	}
+        public ContentContext Navigate(string path)
+        {
+            var explorer = Explorer.Navigate(path);
+            if (explorer == null)
+                return null;
+
+            return new ContentContext(this, explorer);
+        }
+    }
 }
--- src/BrandUp.Pages/Extensions/IHtmlHelperExtensions.cs
+++ src/BrandUp.Pages/Extensions/IHtmlHelperExtensions.cs
@@ -1,21 +1,60 @@
-﻿using BrandUp.Pages.Views;
+﻿using BrandUp.Pages.Content;
+using BrandUp.Pages.Views;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BrandUp.Pages
 {
-	public static class IHtmlHelperExtensions
-	{
-		public static async Task<IHtmlContent> RenderPageAsync(this IHtmlHelper<ContentPageModel> htmlHelper)
-		{
-			var viewRenderService = htmlHelper.ViewContext.HttpContext.RequestServices.GetRequiredService<IViewRenderService>();
-			var pageModel = htmlHelper.ViewData.Model;
+    public static class IHtmlHelperExtensions
+    {
+        public static async Task<IHtmlContent> RenderPageAsync(this IHtmlHelper<ContentPageModel> htmlHelper)
+        {
+            var viewRenderService = htmlHelper.ViewContext.HttpContext.RequestServices.GetRequiredService<IViewRenderService>();
+            var pageModel = htmlHelper.ViewData.Model;
 
-			var builder = new HtmlContentBuilder();
-			var pageHtml = await viewRenderService.RenderToStringAsync(pageModel.ContentContext);
-			builder.AppendHtml(pageHtml);
-			return builder;
-		}
-	}
+            var builder = new HtmlContentBuilder();
+            var pageHtml = await viewRenderService.RenderToStringAsync(pageModel.ContentContext);
+            builder.AppendHtml(pageHtml);
+            return builder;
+        }
+
+        /// <summary>
+        /// Ренреринг статического блока.
+        /// </summary>
+        /// <typeparam name="TContent">Тип контента блока.</typeparam>
+        /// <param name="htmlHelper">HTML хелпер представления.</param>
+        /// <param name="key">Ключ блока.</param>
+        /// <returns>HTML контент блока.</returns>
+        public static async Task<IHtmlContent> BlockAsync(this IHtmlHelper htmlHelper, Type contentType, string key)
+        {
+            ArgumentNullException.ThrowIfNull(nameof(htmlHelper));
+            ArgumentException.ThrowIfNullOrWhiteSpace(nameof(contentType));
+            ArgumentException.ThrowIfNullOrWhiteSpace(nameof(key));
+
+            var services = htmlHelper.ViewContext.HttpContext.RequestServices;
+            var contentMetadataManager = services.GetRequiredService<IContentMetadataManager>();
+            var viewLocator = services.GetRequiredService<IViewLocator>();
+            var viewRenderService = services.GetRequiredService<IViewRenderService>();
+
+            if (!contentMetadataManager.TryGetMetadata(contentType, out var contentMetadataProvider))
+                throw new InvalidOperationException("Не зарегистрирован тип контента.");
+
+            var view = viewLocator.FindView(contentMetadataProvider.ModelType);
+            if (view == null)
+                throw new InvalidOperationException();
+
+            var contentModel = contentMetadataProvider.CreateModelInstance();
+
+            if (view.DefaultModelData != null)
+                contentMetadataProvider.ApplyDataToModel(view.DefaultModelData, contentModel);
+
+            var contentContext = new ContentContext(null, contentModel, services, false);
+
+            var builder = new HtmlContentBuilder();
+            var pageHtml = await viewRenderService.RenderToStringAsync(contentContext);
+            builder.AppendHtml(pageHtml);
+            return builder;
+        }
+    }
 }
