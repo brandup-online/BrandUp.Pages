afff7a0899375a2653524fdb8ac4111a44f5250b
Dmitry Kovyazin <it@brandup.online>
2024-06-26 19:42:39 +0700

    редактируемое мобильное меню

---
+++ examples/LandingWebSite/Contents/MobileMenu/MobileMenu.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BrandUp.Pages;
+using BrandUp.Pages.Content;
+using BrandUp.Pages.Content.Fields;
+
+namespace LandingWebSite.Contents.MobileMenu
+{
+    [ContentType]
+    public class MobileMenuContent
+    {
+        [Model(Title = "Меню")]
+        public List<MenuItem> Items { get; set; }
+
+        [ContentType]
+        public class MenuItem
+        {
+            [Text, Title, Required]
+            public string Text { get; set; }
+            [HyperLink, Required]
+            public HyperLinkValue Url { get; set; }
+        }
+    }
+}
