bcdc383ad54b8f11fc8bb44107aa620a41f6f74a
Mikhail Nazarov <misha-nazarov.nazarov@yandex.ru>
2024-06-28 14:50:08 +0700

    #2808

--- src/brandup-ui-pages/source/content/designer/page-blocks.ts
+++ src/brandup-ui-pages/source/content/designer/page-blocks.ts
@@ -1,4 +1,4 @@
-﻿import { ModelDesigner } from "./model";
+﻿import { ModelDesigner, ModelDesignerOptions } from "./model";
 import { DOM } from "brandup-ui-dom";
 import "./page-blocks.less";
 import iconRefresh from "../../svg/new/update.svg";
@@ -7,18 +7,161 @@ import iconDelete from "../../svg/new/trash.svg";
 import iconSort from "../../svg/new/sort.svg";
 import iconSortDown from "../../svg/new/sort-down.svg";
 import iconAdd from "../../svg/page-blocks-add.svg";
+import { IPageDesigner } from "../../typings/content";
+import { DesignerEvent } from "./base";
 
 export class PageBlocksDesigner extends ModelDesigner {
     get typeName(): string { return "BrandUpPages.PageBlocksDesigner"; }
 
-    protected onRender(elem: HTMLElement) {
-        super.onRender(elem);
+    constructor(page: IPageDesigner, elem: HTMLElement, options: ModelDesignerOptions) {
+        super(page, elem, options);
+        this.renderBlocks();
+    }
 
+    protected onRender(elem: HTMLElement) {
+        elem.classList.add("content-designer");
         elem.classList.add("page-blocks-designer");
 
         if (this.options.isListValue) {
             elem.insertAdjacentElement("beforeend", DOM.tag("div", { class: "page-blocks-designer-new-item bp-elem" }, '<div><ol><li><a href="#" data-command="item-add" class="accent">Добавить блок</a></li></ol></div>'));
         }
+
+        this.registerCommand("item-add", (elem: HTMLElement) => {
+            const itemElem = elem.closest("[data-content-path]");
+            this.triggerCallback("add-item", {value: this.getItemIndex(itemElem), target: elem});
+        });
+        this.registerCommand("item-view", () => { return; });
+        this.registerCommand("item-settings", (elem: HTMLElement) => {
+            const itemElem = elem.closest("[data-content-path]");
+            const contentPath = itemElem.getAttribute("data-content-path");
+            const index = this.getItemIndex(itemElem);
+
+            this.triggerCallback("item-settings", {value: { contentPath, index }, target: itemElem as HTMLElement});
+        });
+        this.registerCommand("item-delete", (elem: HTMLElement) => {
+            const itemElem = elem.closest("[data-content-path]");
+            if (itemElem.classList.contains("processing"))
+                return;
+            itemElem.classList.add("processing");
+            const index = this.getItemIndex(itemElem);
+            itemElem.remove();
+            // this._refreshBlockIndexes();
+            this.renderBlocks();
+            this.triggerCallback("item-delete", {value: { index, itemElem }, target: elem});
+        });
+        this.registerCommand("item-up", (elem: HTMLElement) => {
+            const itemElem = elem.closest("[data-content-path]");
+            const itemIndex = this.getItemIndex(itemElem);
+            if (itemIndex <= 0)
+                return;
+
+            if (itemElem.classList.contains("processing"))
+                return;
+            itemElem.classList.add("processing");
+
+            itemElem.previousElementSibling.insertAdjacentElement("beforebegin", itemElem);
+            this._refreshBlockIndexes();
+            this.renderBlocks();
+
+            this.triggerCallback("item-up", { target: itemElem as HTMLElement, value: itemIndex});
+        });
+        this.registerCommand("item-down", (elem: HTMLElement) => {
+            const itemElem = elem.closest("[data-content-path]");
+            const itemIndex = this.getItemIndex(itemElem);
+
+            if (itemIndex + 1 >= this.countItems())
+                return;
+
+            if (itemElem.classList.contains("processing"))
+                return;
+            itemElem.classList.add("processing");
+
+            itemElem.nextElementSibling.insertAdjacentElement("afterend", itemElem);
+            this._refreshBlockIndexes();
+            this.renderBlocks();
+
+            this.triggerCallback("item-down", { target: itemElem as HTMLElement, value: itemIndex});
+        });
+        this.registerCommand("item-refresh", (elem: HTMLElement) => {
+            const itemElem = elem.closest("[data-content-path]");
+            if (itemElem.classList.contains("processing"))
+                return;
+            itemElem.classList.add("processing");
+
+            this.triggerCallback("item-refresh", {target: itemElem as HTMLElement, value: this.getItemIndex(itemElem)});
+        });
+    }
+    protected eachItems(f: (elem: Element, index: number) => void) {
+        const elements = this.getItems();
+        for ( let i=0; i < elements.length; i++ )
+            f(elements.item(i), i);
+    }
+    protected countItems(): number {
+        let i = 0;
+        this.eachItems(() => i++);
+        return i;
+    }
+    protected getItemIndex(item: Element) {
+        const items = this.getItems();
+        for (let i = 0; i < items.length; i++) {
+            if (items.item(i) === item) return i;
+        }
+        return -1;
+    }
+    protected getItems () {
+        return DOM.queryElements(this.element, `.page-blocks-designer > [data-content-path]`);
+    }
+    protected getItem(index: number): Element {
+        let itemElem: Element;
+
+        for (let i = 0; i < this.element.children.length; i++) {
+            itemElem = this.element.children.item(i);
+            if (itemElem.hasAttribute("data-content-path") && i === index)
+                return itemElem;
+        }
+
+        return itemElem;
+    }
+    addItem (data, index) {
+        const fragment = document.createDocumentFragment();
+        const container = DOM.tag("div", null, data);
+        fragment.appendChild(container);
+        const newElem = DOM.queryElement(container, "[data-content-path]");
+
+        if (this.options.isListValue) {
+            if (index > 0)
+                this.getItem(index - 1).insertAdjacentElement("afterend", newElem);
+            else if (index === 0)
+                this.element.insertAdjacentElement("afterbegin", newElem);
+            else
+                this.getItem(-1).insertAdjacentElement("afterend", newElem);
+        }
+
+        this.page.redraw();
+
+        this._renderBlock(newElem);
+
+        this._refreshBlockIndexes();
+    }
+
+    renderBlocks() {
+        this.eachItems((elem) => { this._renderBlock(elem as HTMLElement); });
+    }
+    protected _refreshBlockIndexes() {
+        this.eachItems((elem, index) => { elem.setAttribute("data-content-path-index", index.toString()); });
+    }
+
+    refreshItem(elem: Element, content: any) {
+        const fragment = document.createDocumentFragment();
+        const container = DOM.tag("div", null, content);
+        fragment.appendChild(container);
+
+        const newElem = DOM.queryElement(container, "[data-content-path]");
+        elem.insertAdjacentElement("afterend", newElem);
+        elem.remove();
+
+        this._renderBlock(newElem);
+        this.page.redraw();
     }
 
     protected _renderBlock(blockElem: HTMLElement) {
@@ -39,8 +182,6 @@ export class PageBlocksDesigner extends ModelDesigner {
                 }
             }
         }
-
-        super._renderBlock(blockElem);
 
         var type = blockElem.dataset.contentType;;
 
@@ -68,11 +209,33 @@ export class PageBlocksDesigner extends ModelDesigner {
             '</ul>'));
     }
 
+    setCallback(name: CallbackType, handler: (e: DesignerEvent<any>) => void) {
+        super.setCallback(name, handler);
+    }
+    protected triggerCallback(name: CallbackType, e: DesignerEvent<any>) {
+        super.triggerCallback(name, e);
+    }
+    removeCallback(name:CallbackType) {
+        super.removeCallback(name);
+    }
+
+    hasValue(): boolean {
+        for (let i = 0; i < this.element.children.length; i++) {
+            const itemElem = this.element.children.item(i);
+            if (itemElem.hasAttribute("data-content-path"))
+                return true;
+        }
+        return false;
+    }
+
     destroy() {
         DOM.queryElements(this.element, "* > [data-content-path-index] .page-blocks-designer-item-add").forEach((elem) => { elem.remove(); });
         DOM.queryElements(this.element, "* > [data-content-path-index] .page-blocks-designer-item-tools").forEach((elem) => { elem.remove(); });
         DOM.queryElements(this.element, "* > .page-blocks-designer-new-item").forEach((elem) => { elem.remove(); });
 
+        this.element.classList.remove("content-designer");
         super.destroy();
     }
 }
+
+type CallbackType = "add-item" | "item-settings" | "item-delete" | "item-refresh" | "item-up" | "item-down";
--- src/brandup-ui-pages/source/content/provider/page-blocks.ts
+++ src/brandup-ui-pages/source/content/provider/page-blocks.ts
@@ -1,6 +1,117 @@
+import { AjaxResponse, AjaxRequest } from "brandup-ui-ajax";
+import { selectContentType } from "../../dialogs/dialog-select-content-type";
+import { editPage } from "../../dialogs/pages/edit";
 import { PageBlocksDesigner } from "../designer/page-blocks";
 import { ModelFieldProvider } from "./model";
 
 export class PageBlocksFieldProvider extends ModelFieldProvider {
     protected __designerType = PageBlocksDesigner;
+
+    private __items: any[];
+    
+    createDesigner() {
+        this.__items = this.model.options.isListValue ? this.model.value.items : [];
+        const designer = new this.__designerType(this.__editor, this.__valueElem, this.model.options);
+        designer.setCallback("add-item", (e) => {
+            const elem = e.target;
+            const itemType = elem.getAttribute("data-item-type");
+            let itemIndex = e.value + 1;
+
+            if (!itemType) {
+                selectContentType(this.model.options.itemTypes).then((type) => {
+                    this.addItem(type.name, itemIndex);
+                    
+                });
+            }
+            else
+                this.addItem(itemType, itemIndex);
+        });
+
+        designer.setCallback("item-settings", (e) => {
+            editPage(this.__editor.editId, e.value.contentPath).then(() => {
+                this.__refreshItem(e.target, e.value.index);
+            }).catch(() => {
+                this.__refreshItem(e.target, e.value.index);
+            });
+        })
+
+        designer.setCallback("item-delete", (e) => {
+            this.request({
+                url: '/brandup.pages/content/model',
+                urlParams: { itemIndex: e.value.index.toString() },
+                method: "DELETE",
+            });
+        })
+
+        designer.setCallback("item-refresh", (e) => {
+            this.__refreshItem(e.target, e.value);
+        })
+        designer.setCallback("item-up", (e) => {
+            this.request({
+                url: '/brandup.pages/content/model/up',
+                urlParams: { itemIndex: e.value },
+                method: "POST",
+                success: () => e.target.classList.remove("processing")
+            });
+        })
+        designer.setCallback("item-down", (e) => {
+            this.request({
+                url: '/brandup.pages/content/model/down',
+                urlParams: { itemIndex: e.value },
+                method: "POST",
+                success: () => e.target.classList.remove("processing")
+            });
+        })
+        return designer;
+    }
+
+    protected __refreshItem(elem: Element, index?: number) {
+        const urlParams = { itemIndex: index?.toString() };
+
+        this.request({
+            url: '/brandup.pages/content/model/view',
+            urlParams: urlParams,
+            method: "GET",
+            success: (response: AjaxResponse<string>) => {
+                (this.designer as PageBlocksDesigner).refreshItem(elem, response.data);
+            }
+        });
+    }
+
+    request(options: AjaxRequest) {
+        if (!options.urlParams)
+            options.urlParams = {};
+
+        options.urlParams["editId"] = this.__editor.editId;
+        options.urlParams["path"] = this.__valueElem.dataset.contentFieldPath;
+        options.urlParams["field"] = this.model.name;
+
+        this.__editor.queue.push(options);
+    }
+
+    addItem(itemType: string, index: number) {
+        this.request({
+            url: '/brandup.pages/content/model',
+            urlParams: {
+                itemType: itemType,
+                itemIndex: index.toString()
+            },
+            method: "PUT",
+            success: (response: AjaxResponse) => {
+                if (response.status === 200) {
+                    this.__items = response.data.value.items;
+                    this.request({
+                        url: '/brandup.pages/content/model/view',
+                        urlParams: { itemIndex: index.toString() },
+                        method: "GET",
+                        success: (response: AjaxResponse<string>) => {
+                            if (response.status === 200) {
+                                (this.designer as PageBlocksDesigner).addItem(response.data, index);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
 }
